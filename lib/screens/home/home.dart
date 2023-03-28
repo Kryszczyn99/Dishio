4358fef3748069d60a740b157a35a785a280a440
Kryszczyn99 <59519646+Kryszczyn99@users.noreply.github.com>
2023-03-29 00:41:28 +0200

    poprawione logowanie i wylogowywanie

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:dishio/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -12,10 +13,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final AuthService auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Siema"),
+    return Scaffold(
+      backgroundColor: MyColors.color3,
+      appBar: AppBar(
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.white,
+            height: 1.0,
+          ),
+          preferredSize: Size.fromHeight(4.0),
+        ),
+        backgroundColor: MyColors.color5,
+        elevation: 0.0,
+        centerTitle: true,
+        leadingWidth: 60,
+        actions: <Widget>[
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              primary: MyColors.color5,
+            ),
+            onPressed: () async {
+              Navigator.of(context).popUntil((route) => route.isFirst);
+              await auth.signOut();
+              //scheduleNotificationsTest();
+            },
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            label: Text('logout', style: TextStyle(color: Colors.white)),
+          ),
+        ],
+        title: Text(
+          "Home",
+          style: TextStyle(fontSize: 34, fontStyle: FontStyle.italic),
+        ),
+      ),
+      body: Container(),
     );
   }
 }
